80afca038e06284be8eb3acb85770236ac290a46
WinfredSU <winfredsu@outlook.com>
2017-01-13 15:07:45 +0800

    initial commit

---
+++ src/js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+ 
+  $("#sponsor-list").owlCarousel({
+    //autoPlay: 3000, //Set AutoPlay to 3 seconds
+    //stopOnHover: true,
+    items : 5,
+    itemsDesktop: [1200,3],
+    itemsDesktopSmall: [991,3],
+    itemsTablet: [767,2],
+    itemsTabletSmall: [625,2],
+    itemsMobile: [479,1]      
+  });
+});
